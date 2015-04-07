091f74825f97e431301e1ead8eca02781bb9e9b2
Gabriele Petronella <gabriele@buildo.io>
2015-04-07 16:24:19 +0200

    Fix dependendencies, make react a direct dependency, minimize webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,6 @@
 'use strict';
 
-var _ = require('lodash');
 var webpack = require('webpack');
-var argv = require('minimist')(process.argv.slice(2));
-var path = require('path');
-
-var DEBUG = !argv.release;
-
-var AUTOPREFIXER_LOADER = 'autoprefixer-loader?{browsers:[' +
-  '"Android 2.3", "Android >= 4", "Chrome >= 20", "Firefox >= 24", ' +
-  '"Explorer >= 8", "iOS >= 6", "Opera >= 12", "Safari >= 6"]}';
-
-var GLOBALS = {
-  'process.env.NODE_ENV': DEBUG ? '"development"' : '"production"',
-  '__DEV__': DEBUG
-};
 
 var config = {
 
@@ -27,18 +13,18 @@ var config = {
     libraryTarget: 'umd'
   },
 
-  cache: DEBUG,
-  debug: DEBUG,
+  cache: false,
+  debug: true,
 
   stats: {
     colors: true,
-    reasons: DEBUG
+    reasons: true
   },
 
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
+    // new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    //new webpack.optimize.UglifyJsPlugin({minimize: true})
+    // new webpack.optimize.UglifyJsPlugin({minimize: true})
   ],
 
   resolve: {
@@ -48,19 +34,7 @@ var config = {
   module: {
     loaders: [{
       test: /\.css$/,
-      loader: 'style-loader!css-loader!' + AUTOPREFIXER_LOADER
-    }, {
-      test: /\.gif$/,
-      loader: 'url-loader?limit=10000&mimetype=image/gif'
-    }, {
-      test: /\.jpg$/,
-      loader: 'url-loader?limit=10000&mimetype=image/jpg'
-    }, {
-      test: /\.png$/,
-      loader: 'url-loader?limit=10000&mimetype=image/png'
-    }, {
-      test: /\.svg$/,
-      loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+      loader: 'style-loader!css-loader!'
     }, {
       test: /\.jsx?$/,
       exclude: /node_modules/,
