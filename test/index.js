a4539b48c5239ee52cb9ace673c6f3dade856411
Francesco Cioria <firefelix@gmail.com>
2017-03-01 10:58:46 +0100

    fix lint errors

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ const requireDir = require('require-dir');
 require('babel-register')({
   ignore: /node_modules/,
   extensions: ['.js', '.jsx'],
-  presets: [ 'es2015', 'react', 'stage-0' ],
+  presets: ['es2015', 'react', 'stage-0'],
   plugins: [
     'transform-decorators-legacy',
     'lodash'
