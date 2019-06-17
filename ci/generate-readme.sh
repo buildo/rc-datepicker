39cc2a204709581ee768752ae003eab87610ce18
Eric Camellini <eric.camellini@buildo.io>
2019-06-17 17:30:53 +0200

    Fixing if

--- ci/generate-readme.sh
+++ ci/generate-readme.sh
@@ -8,10 +8,14 @@ apt-get install -qy git
 
 yarn install --no-progress
 yarn generate-readme
-git add src/README.md
-if !git diff-index --quiet HEAD -- src/README.md; then
+
+if ! git diff-index --quiet HEAD -- src/README.md
+then
+  git add src/README.md;
   git config --global user.name "nemobot";
   git config --global user.email "our-bots@buildo.io";
   git commit -m "Update README [skip CI]";
   git push origin master;
+else
+  exit 0
 fi
