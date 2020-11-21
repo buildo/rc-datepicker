76089d561178b41b75a7bda526b86e9f658f5feb
Baptiste Augrain <daiyam@zokugun.org>
2020-11-21 12:40:18 +0100

    onChange is optional

--- src/utils/valueLink.js
+++ src/utils/valueLink.js
@@ -1,9 +1,12 @@
+function noop() {
+}
+
 export default function format(Component) {
   Component.prototype.getValueLink = function(_props) {
     const props = _props || this.props;
     return props.valueLink || {
       value: props.value,
-      requestChange: props.onChange
+      requestChange: props.onChange || noop
     };
   };
 }
